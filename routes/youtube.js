2c409020bed4edb8808c2f093dc281de7e262c0e
Ricky <audrickong.ao@gmail.com>
2019-10-04 11:46:34 +0700

    Push

--- routes/youtube.js
+++ routes/youtube.js
@@ -2,7 +2,7 @@ const YoutubeController = require('../controllers/youtube')
 const express = require('express')
 const router = express.Router()
 
-router.get('/video',YoutubeController.getVideo)
+router.post('/video',YoutubeController.getVideo)
 router.post('/comment',YoutubeController.commentVideo)
 
 module.exports = router
