edfb3b4e46336724635672fddbef4e5918427bb8
uzaisindiko <uzai.sindiko@gmail.com>
2019-10-04 11:45:33 +0700

    addFavorite

--- model/user.js
+++ model/user.js
@@ -9,18 +9,35 @@ const UserSchema = new Schema({
     email: {
         type: String,
         required: [true, 'email is required'],
-        validate: {
-            validator: function (v) {
-                return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
+        validate: [
+            {
+                validator: function (value) {
+                    let emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+                    return emailRegex.test(value);
+                },
+                message: props => `${props.value} is not a valid email!`
             },
-            message: props => `${props.value} is not a valid email!`
-        },
+            {
+                validator: function (value) {
+                    return this.model('Member').findOne({ email: value })
+                        .then(email => {
+                            if (email) {
+                                return false
+                            } else {
+                                return true
+                            }
+                        })
+                },
+                message: props => `${props.value} has already been used by another user`
+            }
+        ],
     },
     password: {
         type: String,
         required: [true, 'password is required'],
         default: 'PASSWORD'
-    }
+    },
+    favoriteHeros: Array
 })
 
 const User = mongoose.model('User', UserSchema);
